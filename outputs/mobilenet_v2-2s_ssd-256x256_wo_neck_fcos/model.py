41921354dc304bb5a776db54f64246881e8f3e21
LongaBonga <ivan.dobryaev@gmail.com>
2021-08-13 14:50:55 +0300

    change path to config

---
+++ outputs/mobilenet_v2-2s_ssd-256x256_wo_neck_fcos/model.py
@@ -0,0 +1,176 @@
+input_size = 256
+image_width = 256
+image_height = 256
+width_mult = 1.0
+model = dict(
+    type='SingleStageDetector',
+    backbone=dict(
+        type='mobilenetv2_w1',
+        out_indices=(4, 5),
+        frozen_stages=-1,
+        norm_eval=False,
+        pretrained=True),
+    neck=None,
+    bbox_head=dict(
+        type='FCOSHead',
+        num_classes=80,
+        in_channels=96,
+        feat_channels=64,
+        strides=[16, 32],
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='GIoULoss', loss_weight=2.0),
+        loss_centerness=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)),
+    train_cfg=dict(
+        assigner=dict(type='ATSSAssigner', topk=9),
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False),
+    test_cfg=dict(
+        nms=dict(type='nms', iou_threshold=0.45),
+        min_bbox_size=0,
+        score_thr=0.02,
+        max_per_img=200,
+        nms_pre_classwise=200))
+cudnn_benchmark = True
+dataset_type = 'CocoDataset'
+img_norm_cfg = dict(mean=[0, 0, 0], std=[255, 255, 255], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile', to_float32=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PhotoMetricDistortion',
+        brightness_delta=32,
+        contrast_range=(0.5, 1.5),
+        saturation_range=(0.5, 1.5),
+        hue_delta=18),
+    dict(
+        type='MinIoURandomCrop',
+        min_ious=(0.1, 0.3, 0.5, 0.7, 0.9),
+        min_crop_size=0.1),
+    dict(type='Resize', img_scale=(256, 256), keep_ratio=False),
+    dict(type='Normalize', mean=[0, 0, 0], std=[255, 255, 255], to_rgb=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(256, 256),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=False),
+            dict(
+                type='Normalize',
+                mean=[0, 0, 0],
+                std=[255, 255, 255],
+                to_rgb=True),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=64,
+    workers_per_gpu=4,
+    train=dict(
+        type='RepeatDataset',
+        times=5,
+        dataset=dict(
+            type='CocoDataset',
+            ann_file='./data/coco/annotations/instances_train2017.json',
+            img_prefix='./data/coco/images/train2017',
+            pipeline=[
+                dict(type='LoadImageFromFile', to_float32=True),
+                dict(type='LoadAnnotations', with_bbox=True),
+                dict(
+                    type='PhotoMetricDistortion',
+                    brightness_delta=32,
+                    contrast_range=(0.5, 1.5),
+                    saturation_range=(0.5, 1.5),
+                    hue_delta=18),
+                dict(
+                    type='MinIoURandomCrop',
+                    min_ious=(0.1, 0.3, 0.5, 0.7, 0.9),
+                    min_crop_size=0.1),
+                dict(type='Resize', img_scale=(256, 256), keep_ratio=False),
+                dict(
+                    type='Normalize',
+                    mean=[0, 0, 0],
+                    std=[255, 255, 255],
+                    to_rgb=True),
+                dict(type='RandomFlip', flip_ratio=0.5),
+                dict(type='DefaultFormatBundle'),
+                dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+            ])),
+    val=dict(
+        type='CocoDataset',
+        ann_file='./data/coco/annotations/instances_val2017.json',
+        img_prefix='./data/coco/images/val2017',
+        test_mode=True,
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(256, 256),
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=False),
+                    dict(
+                        type='Normalize',
+                        mean=[0, 0, 0],
+                        std=[255, 255, 255],
+                        to_rgb=True),
+                    dict(type='ImageToTensor', keys=['img']),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ]),
+    test=dict(
+        type='CocoDataset',
+        ann_file='./data/coco/annotations/instances_val2017.json',
+        img_prefix='./data/coco/images/test2017',
+        test_mode=True,
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(256, 256),
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=False),
+                    dict(
+                        type='Normalize',
+                        mean=[0, 0, 0],
+                        std=[255, 255, 255],
+                        to_rgb=True),
+                    dict(type='ImageToTensor', keys=['img']),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ]))
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict()
+lr_config = dict(
+    policy='step',
+    warmup='constant',
+    warmup_iters=500,
+    warmup_ratio=0.3333333333333333,
+    step=[14, 22])
+checkpoint_config = dict(interval=1)
+log_config = dict(
+    interval=50,
+    hooks=[dict(type='TextLoggerHook'),
+           dict(type='TensorboardLoggerHook')])
+total_epochs = 30
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+work_dir = 'outputs/mobilenet_v2-2s_ssd-256x256_wo_neck_fcos'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+gpu_ids = range(0, 1)
